2def609c8757c946d3cdef6079ac1f844fe0c3cb
zzacal <jizacal@gmail.com>
2022-01-16 12:26:23 -1000

    Styled Messages component

--- src/Messages/Models/MessageViewModel.cs
+++ src/Messages/Models/MessageViewModel.cs
@@ -3,6 +3,6 @@ public class MessageViewModel : IMessage
 {
     public IEnumerable<MessageData> Messages { get; set; } =  new List<MessageData>();
     public bool IsSubmitted { get; set; }
-    public int Limit { get; set; } = 280;
+    public int Limit { get; set; } = 500;
     public string Body { get; set; }  = "";
 }
