032469da65b98c2716002b5ffcb387cd3799ec6c
zzacal <jizacal@gmail.com>
2022-01-19 22:45:33 -0800

    Corrected time stamps and timezone info

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Wow! Such vanilla!
+const timeStamps = document.getElementsByClassName("message-created");
+Array.from(timeStamps).forEach(e => {
+  try {
+    const timeAtt = e.getAttribute("time");  
+    const date = new Date(timeAtt);  
+    const localizedDate = date.toLocaleString();
+    
+    e.innerHTML = localizedDate;
+  } catch (error) {
+    console.log(error);
+  }
+});
